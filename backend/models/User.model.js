782b898f1ead4ebe6a1532183f3d061191593efb
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-08-26 19:15:29 -0300

    user model

---
+++ backend/models/User.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now(),
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    verifcationToken: String,
+    verifcationTokenExpires: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
