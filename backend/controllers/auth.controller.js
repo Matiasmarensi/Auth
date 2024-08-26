103b39a39f5796c0886c1632744e1bf4106142f4
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-08-26 19:08:21 -0300

    auth router

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.send("Hello World");
+};
+export const login = async (req, res) => {
+  res.send("Hello World");
+};
+export const logout = async (req, res) => {
+  res.send("Hello World");
+};
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
